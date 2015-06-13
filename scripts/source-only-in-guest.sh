96b4988fd376483943b8baae41fbe4a1ce20dad6
neomatrix369 <sadhak001@gmail.com>
2015-06-13 09:21:56 +0100

    Refactoring common code across the two source...sh into its own script file

--- scripts/source-only-in-guest.sh
+++ scripts/source-only-in-guest.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 
-export SOURCE_CODE=${HOME}/sources ;
-JDK_FOLDER=jdk9
-OPENJDK_REPO=http://hg.openjdk.java.net/jdk9/dev
-SCRIPTS_FOLDER=../../scripts/
+set -eu
 
-mkdir -p ${SOURCE_CODE} ;
-cd ${SOURCE_CODE} ;
+BASEDIR=$(dirname $0)
 
-# if jdk9 already exists don't clone
-if [ ! -d ${SOURCE_CODE}/${JDK_FOLDER} ] ; then
-	hg clone ${OPENJDK_REPO} ${JDK_FOLDER} ;
-fi
-
-# update and double check clone worked
-cd ${SOURCE_CODE}/${JDK_FOLDER} ;
-hg update ;
-
-chmod u+x get_source.sh ;
-
-./get_source.sh ;
-
-cp -f ${SCRIPTS_FOLDER}/update*.sh ${SOURCE_CODE}/${JDK_FOLDER}
+sh $BASEDIR/source-common.sh $HOME
--- scripts/source-share-with-host.sh
+++ scripts/source-share-with-host.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
 
+set -eu
+
 VAGRANT_HOST_GUEST_SHARED_FOLDER=/vagrant
-export SOURCE_CODE=${VAGRANT_HOST_GUEST_SHARED_FOLDER}/sources ;
-JDK_FOLDER=jdk9
-OPENJDK_REPO=http://hg.openjdk.java.net/jdk9/dev
-SCRIPTS_FOLDER=../../scripts/
+BASEDIR=$(dirname $0)
 
-mkdir -p ${SOURCE_CODE} ;
-cd ${SOURCE_CODE} ;
-
-# if jdk9 already exists don't clone
-if [ ! -d ${SOURCE_CODE}/${JDK_FOLDER} ] ; then
-	hg clone ${OPENJDK_REPO} ${JDK_FOLDER} ;
-fi
-
-# update and double check clone worked
-cd ${SOURCE_CODE}/${JDK_FOLDER} ;
-hg update ;
-
-chmod u+x get_source.sh ;
-
-./get_source.sh ;
-
-cp -f ${SCRIPTS_FOLDER}/update*.sh ${SOURCE_CODE}/${JDK_FOLDER}
+sh $BASEDIR/source-common.sh $VAGRANT_HOST_GUEST_SHARED_FOLDER
