bc7726fc6ac1408f468f204fa16e94c135f5fb39
neomatrix369 <sadhak001@gmail.com>
2015-10-10 11:13:23 +0100

    Amended configure...sh, removed build specific setting. Merged two scripts into one, calling it setupEnvAndBuildOpenJDK9.sh

--- scripts/configureAndBuildOpenJDKOnly.sh
+++ scripts/configureAndBuildOpenJDKOnly.sh
@@ -1,4 +1,2 @@
-JAVA8_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
-
 bash configure $BASH_CONFIGURE_PARAMS
 make $MAKE_PARAMS
--- scripts/setupEnvForOpenJDK9.sh
+++
@@ -1,6 +0,0 @@
-export JDK_FOLDER=jdk9
-export OPENJDK_REPO=http://hg.openjdk.java.net/jdk9/dev
-### This is the JDK8 path inside the vagrant box
-export JAVA8_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
-export BASH_CONFIGURE_PARAMS="--with-boot-jdk=$JAVA8_HOME --disable-warnings-as-errors"
-export MAKE_PARAMS="images LOG=debug"
